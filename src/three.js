867f57ce168b387224ec310389c7e1ced5355de6
Avaer Kazmer <a@modules.io>
2021-05-09 11:29:09 -0400

    Update three.js

--- src/three.js
+++ src/three.js
@@ -1,6 +1,8 @@
 // import './node.js';
 
 export * from '../three.module.js';
-export * as THREE from '../three.module.js';
+import * as THREE from '../three.module.js';
+export default THREE;
 export {GLTFLoader} from '../GLTFLoader.js';
 export {GLTFExporter} from '../GLTFExporter.js';
+export {BufferGeometryUtils} from '../BufferGeometryUtils.js';
