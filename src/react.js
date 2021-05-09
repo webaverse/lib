45e032302c0c1433805597df6ebfbc0d0540c268
Avaer Kazmer <a@modules.io>
2021-05-09 12:16:10 -0400

    Update react.js

--- src/react.js
+++ src/react.js
@@ -4,3 +4,13 @@ export * from 'react';
 export {Element, Fragment, Suspense, useState, useEffect, useRef} from 'react';
 import React from 'react';
 export default React;
+
+export * from 'react-dom';
+// export {Element, Fragment, useState} from 'react-dom';
+import ReactDOM from 'react-dom';
+export default ReactDOM;
+
+export * from '@react-three/fiber';
+export {Canvas, useFrame, useThree, useLoader} from '@react-three/fiber';
+import * as ReactThreeFiber from '@react-three/fiber';
+export default ReactThreeFiber;
