3cfa6ca8a09f60170d425383c5622cabfc2a4652
Avaer Kazmer <a@modules.io>
2021-05-09 05:22:54 -0400

    Create react-all.js

---
+++ src/react-all.js
@@ -0,0 +1,21 @@
+import './node.js';
+
+export * from 'three';
+// export {Element, Fragment, useState, useEffect, useRef} from 'react';
+export * as THREE from 'three';
+
+export {GLTFLoader} from '../GLTFLoader.js';
+
+export * from 'react';
+export {Element, Fragment, Suspense, useState, useEffect, useRef} from 'react';
+export {default as React} from 'react';
+
+// import './node.js';
+export * from 'react-dom';
+// export {Element, Fragment, useState} from 'react-dom';
+export {default as ReactDOM} from 'react-dom';
+
+// import './node.js';
+export * from '@react-three/fiber';
+export {Canvas, useFrame, useThree, useLoader} from '@react-three/fiber';
+export * as ReactThreeFiber from '@react-three/fiber';
