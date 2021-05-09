5ea88233be4828ffaefaf98f4c13cccadca5aa4d
Avaer Kazmer <a@modules.io>
2021-05-09 11:08:47 -0400

    Create react-dom.js

---
+++ src/react-dom.js
@@ -0,0 +1,7 @@
+// import './node.js';
+
+// import './node.js';
+export * from 'react-dom';
+// export {Element, Fragment, useState} from 'react-dom';
+import ReactDOM from 'react-dom';
+export default ReactDOM;
