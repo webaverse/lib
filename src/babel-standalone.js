a79245cfb05fa498ec3d067316f00af018eadb86
Avaer Kazmer <a@modules.io>
2021-05-09 13:35:48 -0400

    Create babel-standalone.js

---
+++ src/babel-standalone.js
@@ -0,0 +1,6 @@
+// import './node.js';
+
+export * from '@babel/standalone';
+// export {Element, Fragment, useState, useEffect, useRef} from '@babel/standalone';
+import BabelStandalone from '@babel/standalone';
+export default BabelStandalone;
