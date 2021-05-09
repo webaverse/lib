f544fecd7e83e5b10f6175c86e1e2254c1367b63
Avaer Kazmer <a@modules.io>
2021-05-09 05:34:43 -0400

    Checkpoint split build

---
+++ src/compile.js
@@ -0,0 +1,5 @@
+import './node.js';
+
+export * from '@babel/standalone';
+// export {Element, Fragment, useState, useEffect, useRef} from '@babel/standalone';
+export {default} from '@babel/standalone';
