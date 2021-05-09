ff96c06bb4480f3fa4cdfc344bf93efa3fee3273
Avaer Kazmer <a@modules.io>
2021-05-09 11:08:49 -0400

    Create react-three-fiber.js

---
+++ src/react-three-fiber.js
@@ -0,0 +1,5 @@
+// import './node.js';
+
+export * from '@react-three/fiber';
+export {Canvas, useFrame, useThree, useLoader} from '@react-three/fiber';
+export * as ReactThreeFiber from '@react-three/fiber';
