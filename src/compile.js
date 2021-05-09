db789927c2a940839e6b9cab5c78d705e993926c
Avaer Kazmer <a@modules.io>
2021-05-09 13:35:50 -0400

    Update compile.js

--- src/compile.js
+++ src/compile.js
@@ -4,9 +4,9 @@ export * from '@babel/standalone';
 // export {Element, Fragment, useState, useEffect, useRef} from '@babel/standalone';
 import BabelStandalone from '@babel/standalone';
 export {BabelStandalone};
-console.log('got babel standalone', BabelStandalone);
+// console.log('got babel standalone', BabelStandalone);
 
 export * from '../jszip.js';
 import JSZip from '../jszip.js';
-console.log('got jszip', JSZip);
+// console.log('got jszip', JSZip);
 export {JSZip};
