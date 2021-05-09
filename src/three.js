3393d092d4d9f53cc0f25e834a3b0c02ff4c6209
Avaer Kazmer <a@modules.io>
2021-05-09 11:08:43 -0400

    Create three.js

---
+++ src/three.js
@@ -0,0 +1,6 @@
+// import './node.js';
+
+export * from '../three.module.js';
+export * as THREE from '../three.module.js';
+export {GLTFLoader} from '../GLTFLoader.js';
+export {GLTFExporter} from '../GLTFExporter.js';
